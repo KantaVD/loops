4f8a779372ba27eb878dc0da288892ab3bc5a78a
KantaVD <89583709+KantaVD@users.noreply.github.com>
2021-10-15 17:58:42 +0200

    Add files via upload

---
+++ multiplication-tables.js
@@ -0,0 +1,12 @@
+for (let i = 1; i <=10; i++) {
+    console.log (i + " X 9 = " + (i*9));
+};
+
+//BONUS
+
+for (let a = 1; a <=10; a++) {
+    for (let b = 1; b <=10; b++) {
+        console.log (b + " X " + a + " = " + (a * b));
+    };
+};
+
